06ca72ead6684a5d034ed6047feb23654f486cef
Bryan Ling <66913969+bryanlingc11@users.noreply.github.com>
2020-08-03 00:16:27 +0800

    by bryan

---
+++ app/src/main/java/com/example/ruoqi/ruoqi.java
@@ -0,0 +1,14 @@
+package com.example.ruoqi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ruoqi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ruoqi);
+    }
+}
